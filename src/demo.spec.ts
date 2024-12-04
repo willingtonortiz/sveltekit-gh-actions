0cde8a569a28a26dc819d92034180f5002c82a7c
Willington Ortiz <willingtonortizmaurtua@hotmail.com>
2024-12-04 16:27:24 -0500

    Update tests

--- src/demo.spec.ts
+++ src/demo.spec.ts
@@ -4,4 +4,8 @@ describe('sum test', () => {
   it('adds 1 + 2 to equal 3', () => {
     expect(1 + 2).toBe(3);
   });
+
+  it('adds 1 + 2 to equal 4', () => {
+    expect(1 + 2).toBe(4);
+  });
 });
